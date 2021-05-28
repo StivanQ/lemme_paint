9a7b1bc53ce2ec116abd61dca4567f14a00abe20
StivanQ <patras.fabi@gmail.com>
2021-05-29 01:13:25 +0300

    lemme paint v0.1

--- sensors_and_screen_test.c/sensors_and_screen_test.c.ino
+++ sensors_and_screen_test.c/sensors_and_screen_test.c.ino
@@ -2,15 +2,28 @@
 #include <TFT.h>  
 #include <SPI.h>
 
-// pins
-#define ECHO1 2
-#define TRIG1 3
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEFINES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+// sensor pins
 
-#define ECHO2 4
-#define TRIG2 5
+// sensor 0
+#define ECHO0 2
+#define TRIG0 3
 
-#define ECHO_OFFSET 0
-#define TRIG_OFFSET 1
+// sensor 1
+#define ECHO1 5
+#define TRIG1 4
+
+// sensor 2
+#define ECHO2 6
+#define TRIG2 7
+
+// sensor 3
+#define ECHO3 A1
+#define TRIG3 A0
+
+// sensor 4
+#define ECHO4 A2
+#define TRIG4 A3
 
 // pin definition for Arduino UNO
 #define cs   10
@@ -22,7 +35,7 @@
 #define COLOR_EMPTY 0
 
 // numbers - constants
-#define NO_OF_SENSORS 2
+#define NO_OF_SENSORS 5
 #define NO_OF_MEASUREMENTS 5
 
 // max distance = 40 cm??
@@ -32,20 +45,56 @@
 #define swap(a,b) (a) ^= (b); (b) ^= (a); (a) ^= (b);
 #define sort(a,b) if((a)>(b)){ swap((a),(b)); }
 
+#define MIN_2(a, b) (a) < (b) ? (a) : (b)
+
+#define MIN_3(a, b, c) (a) < (b) ? \
+                        ((a) < (c) ? (a) : (c)) : \
+                        ((b) < (c) ? (b) : (c))
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ VARIABLES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// echo pins
+int echo_pins[] = {ECHO0, ECHO1, ECHO2, ECHO3, ECHO4};
+
+// trig pins
+int trig_pins[] = {TRIG0, TRIG1, TRIG2, TRIG3, TRIG4};
 
 int measurements[NO_OF_SENSORS][NO_OF_MEASUREMENTS];
 int results[NO_OF_SENSORS];
 int last_results[NO_OF_SENSORS];
 
+int history_x[NO_OF_MEASUREMENTS];
+int history_y[NO_OF_MEASUREMENTS];
+
+int crt_history = 0;
+
 int screen_width;
 int screen_height;
 
-unsigned int last_distance1 = 0;
-unsigned int last_distance2 = 0;
+int crt_x;
+int crt_y;
+
+int last_x;
+int last_y;
 
 // create an instance of the library
 TFT TFTscreen = TFT(cs, dc, rst);
-int x = 6;
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ SET UP FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void init_sensors_pins()
+{
+        for (int i = 0; i < NO_OF_SENSORS; i++) {
+                pinMode(echo_pins[i], INPUT);
+                pinMode(trig_pins[i], OUTPUT);
+        }
+
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ MEASUREMENT FUCNTIONS ~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 void clear_measurements()
 {
@@ -68,9 +117,6 @@ void median(int arr[])
         sort(arr[2], arr[3]);
         sort(arr[1], arr[4]);
         sort(arr[1], arr[2]);
-        
-        // this last one is obviously unnecessary for the median
-        // sort(d,e);
 }
 
 // reads all the sensors five times and gets the median for each sensors
@@ -82,8 +128,8 @@ void read_sensors()
         
                 // read each sensor back-to-back
                 for (int j = 0; j < NO_OF_SENSORS; j++) {
-                        // sensors are indexed from 1 to NO_OF_SENSORS
-                        measurements[j][i] = sensor_distance(j + 1);
+                        // sensors are indexed from 0 to NO_OF_SENSORS - 1
+                        measurements[j][i] = sensor_distance(j);
                 }
         }
         
@@ -95,20 +141,6 @@ void read_sensors()
                 // store results
                 results[i] = measurements[i][2];
         }
-
-//  for (int i = 0; i < NO_OF_SENSORS; i++) {
-//    median(measurements[i]);
-//    sprintf(buff, "%c {%d} Valori citite : [%d, %d, %d, %d, %d] -> %d",
-//      i == 0 ? '\t' : ' ',
-//      i,
-//      measurements[i][0],
-//      measurements[i][1],
-//      measurements[i][2],
-//      measurements[i][3],
-//      measurements[i][4],
-//      measurements[i][2]);
-//    Serial.println(buff);
-//  }
 }
 
 void transfers_results_to_old()
@@ -118,22 +150,11 @@ void transfers_results_to_old()
         }
 }
 
-void init_sensors_pins()
-{
-        // sensor number 1
-        pinMode(ECHO1, INPUT);
-        pinMode(TRIG1, OUTPUT);
-        
-        // sensor number 2
-        pinMode(ECHO2, INPUT);
-        pinMode(TRIG2, OUTPUT);
-}
-
 // sensor number i has to have trig pin = 2*i and echo pin 2*i+1
 unsigned int sensor_distance(unsigned int sensor_number)
 {
-        unsigned int trig = sensor_number * 2 + TRIG_OFFSET;
-        unsigned int echo = sensor_number * 2 + ECHO_OFFSET;
+        unsigned int trig = trig_pins[sensor_number];
+        unsigned int echo = echo_pins[sensor_number];
         
         unsigned int distance = 0;
         
@@ -146,9 +167,61 @@ unsigned int sensor_distance(unsigned int sensor_number)
         digitalWrite(trig, LOW);
         
         unsigned long duration = pulseIn(echo, HIGH);
+        // TODO: fine tune me to get distance in pixels, not in quarters
+        // of centimeters
         distance = 2 * duration * 0.034; // instead of cm will be 1/4 of cm
         return distance;
 }
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ DARWING FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/*
+void draw_test_lines(unsigned int distance1, unsigned int distance2)
+{
+        if (last_distance1 == distance1) {
+//               return;
+        } else {
+                TFTscreen.stroke(0, 0, 0);
+                TFTscreen.line(last_distance1, 0, last_distance1, screen_height);
+                last_distance1 = distance1;
+                
+                TFTscreen.stroke(0, 0, 255);
+                TFTscreen.line(distance1, 0, distance1, screen_height);
+        }
+        if (last_distance2 == distance2) {
+        //       return;
+        } else {
+                TFTscreen.stroke(0, 0, 0);
+                TFTscreen.line(last_distance2, 0, last_distance2, screen_height);
+                last_distance2 = distance2;
+                
+                TFTscreen.stroke(0, 255, 0);
+                TFTscreen.line(distance2, 0, distance2, screen_height);
+        }
+}
+
+*/
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEBUG FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void print_measurements()
+{
+
+        char buff[100];
+        sprintf(buff, "s[%d] = [%d] \t"
+                "s[%d] = [%d] \t"
+                "s[%d] = [%d] \t"
+                "s[%d] = [%d] \t"
+                "s[%d] = [%d] \t",
+                0, results[0],
+                1, results[1],
+                2, results[2],
+                3, results[3],
+                4, results[4]);
+        Serial.println(buff);
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ SETUP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 void setup() {
 
@@ -176,47 +249,23 @@ void setup() {
         Serial.begin(9600);
 }
 
-void draw_test_lines(unsigned int distance1, unsigned int distance2)
-{
-        if (last_distance1 == distance1) {
-//               return;
-        } else {
-                TFTscreen.stroke(0, 0, 0);
-                TFTscreen.line(last_distance1, 0, last_distance1, screen_height);
-                last_distance1 = distance1;
-                
-                TFTscreen.stroke(0, 0, 255);
-                TFTscreen.line(distance1, 0, distance1, screen_height);
-        }
-        if (last_distance2 == distance2) {
-        //       return;
-        } else {
-                TFTscreen.stroke(0, 0, 0);
-                TFTscreen.line(last_distance2, 0, last_distance2, screen_height);
-                last_distance2 = distance2;
-                
-                TFTscreen.stroke(0, 255, 0);
-                TFTscreen.line(distance2, 0, distance2, screen_height);
-        }
-}
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 void loop() {
         long t1 = micros();
         read_sensors();
-        
-        unsigned int distance1 = measurements[0][2];
-        unsigned int distance2 = measurements[1][2];
-//        draw_test_lines(distance1, distance2);
-        //  delay(10);
-
-        // drow them points
-        TFTscreen.point(distance1, distance2);
-        
+        crt_x = MIN_3(results[2], results[3], results[4]);
+        crt_y = MIN_2(results[0], results[1]);
+        TFTscreen.circle(crt_x, crt_y, 3);
         long t2 = micros();
 
-        Serial.print(measurements[0][2]);
-        Serial.print("    ");
-        Serial.print(measurements[1][2]);
-        Serial.print("    ");        
-        Serial.println((t2 - t1) / 1000);
+//        Serial.println((t2 - t1) / 1000);
+
+        print_measurements();
+//        Serial.println((t2 - t1) / 1000);
+//        Serial.println(MIN_3(1,2,3));
+//        Serial.println(MIN_3(2,3,1));
+//        Serial.println(MIN_3(3,1,2));
 }
+
+/* EOF */
